72d68e581d6c91b4080a020db8df1120145c1d1c
HornHighYo <95604500+HornHighYo@users.noreply.github.com>
2023-07-31 07:19:01 -0400

    gpt3

--- coverletter2.py
+++ coverletter2.py
@@ -45,13 +45,9 @@ prompt = PromptTemplate(
 
 
 def generate_response(job_details, applicant_details):
-  llm = OpenAI(model_name="gpt-4", temperature=0.7, openai_api_key=openai_api_key)
+  llm = OpenAI(model_name="gpt-3", temperature=0.7, openai_api_key=openai_api_key)
   finalPrompt = prompt.format(job_description=job_details, applicant_description=applicant_details)
-  result = llm(finalPrompt)
-  st.session_state.llm_results[(job_details, applicant_details)] = result
-  st.info(result)
-
-
+  llm(finalPrompt)
 
 # File upload
 # uploaded_file = st.file_uploader('Upload a job description', type='docx')
