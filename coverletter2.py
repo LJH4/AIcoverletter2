e24197ed3165a427947f49293a85b3d676fbd4c6
HornHighYo <95604500+HornHighYo@users.noreply.github.com>
2023-08-08 09:02:12 -0400

    update format to coverletter1

--- coverletter2.py
+++ coverletter2.py
@@ -11,16 +11,21 @@ from langchain import PromptTemplate
 from trubrics.integrations.streamlit import FeedbackCollector
 
 
-#from docx import Document
-
-if "response" not in st.session_state:
-    st.session_state["response"] = ""
-
 st.set_page_config(page_title="Cover Letter Generator")
 st.title("Create Amazing Cover Letters")
 
-#openai_api_key=st.secrets.openai_api_key
-openai_api_key=st.secrets["openai"]["openai_api_key"]
+st.sidebar.title("Get that dream job!")
+#st.sidebar.markdown("[Sign up now](https://subscribepage.io/yJeBNL) to receive early access to additional AI tools")
+st.sidebar.write("Powered by OpenAI, this app serves as your personal assistant to draft compelling cover letters for job applications. Simply copy and paste the job description and your resume into the respective text fields and press submit!") 
+
+URL_STRING = "https://subscribepage.io/yJeBNL"
+st.sidebar.markdown(
+    f'<a href="{URL_STRING}" style="display: inline-block; padding: 12px 20px; background-color: #706dcb; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Sign Up for early access to all our AI tools!</a>',
+    unsafe_allow_html=True
+)
+
+openai_api_key=st.secrets.openai_api_key
+
 
 template = """
 You are an expert in writing cover letters for job applicants.
@@ -45,27 +50,29 @@ prompt = PromptTemplate(
 
 #user_email = st.sidebar.text_input('Provide your email to be first to receive updates and access to new tools:')
 
+
+
 def generate_response(job_details, applicant_details):
   llm = OpenAI(model_name="gpt-4", temperature=0.7, openai_api_key=openai_api_key)
   finalPrompt = prompt.format(job_description=job_details, applicant_description=applicant_details)
-  response = llm(finalPrompt)
-  response_text = response.choices[0].text.replace("\n", "") if hasattr(response, 'choices') else ""
-  return response_text
-  #st.info(llm(finalPrompt))
-  #return llm(finalPrompt)
+  st.info(llm(finalPrompt))
+  #st.write(finalPrompt)
+
+
+
+# File upload
+# uploaded_file = st.file_uploader('Upload a job description', type='docx')
+# Query text
+#query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
+
 
 with st.form('my_form'):
   job_details = st.text_area('Paste the job description here, or write a few sentences about the role.','Role CEO X.AI. Lead the team whose goal is to understand the true nature of the universe.  Report directly to Elon.')
-  applicant_details = st.text_area('Paste your resume here, or write a few sentences about yourself.','Bodybuilder, Conan, Terminator and former governor of California.  I killed the Predator.') 
+  applicant_details = st.text_area('Paste your resume here, or write a few sentences about yourself.','Bodybuilder, Conan, Terminator and former governor of of California.  I killed the Predator.') 
   submitted = st.form_submit_button('Submit')
-
   if submitted and openai_api_key.startswith('sk-'):
-      st.session_state["response"]=generate_response(job_details, applicant_details)
-  
-  if st.session_state["response"]:
-    st.write(f"Response: {st.session_state['response']}")
-    #response_text = st.session_state["response"].choices[0].text.replace("\n", "")
-    
+    generate_response(job_details, applicant_details)
+
 
 collector = FeedbackCollector(
     component_name="evaluate_letter",
